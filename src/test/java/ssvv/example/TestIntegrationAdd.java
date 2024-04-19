5d6669c0fdff30bdc95b210b9cd4615c8aaf7c95
917tapoimarius <marius.tapoi@stud.ubbcluj.ro>
2024-04-20 02:02:20 +0300

    Incremental Integration Done :D

--- src/test/java/ssvv/example/TestIntegrationAdd.java
+++ src/test/java/ssvv/example/TestIntegrationAdd.java
@@ -3,7 +3,6 @@ package ssvv.example;
 import domain.Nota;
 import domain.Student;
 import domain.Tema;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -14,6 +13,7 @@ import service.Service;
 import validation.NotaValidator;
 import validation.StudentValidator;
 import validation.TemaValidator;
+import validation.ValidationException;
 
 import java.time.LocalDate;
 
@@ -84,6 +84,61 @@ public class TestIntegrationAdd {
 
         Nota nota1 = service.findNota("15#100");
         assert(nota1.equals(nota));
+    }
+
+    @Test
+    public void testAddStudentException(){
+        String id = "";
+        String nume = "varga";
+        int grupa = 937;
+        String email = "a@b.com";
+        Student varga = new Student(id,nume,grupa,email);
+
+        try {
+            service.addStudent(varga);
+        }catch (ValidationException v){
+            assert(v.getMessage().equals("Id incorect!"));
+        }
+    }
+
+    @Test
+    public void testAddStudentAndAddAssignmentException(){
+        String id = "15";
+        String nume = "varga";
+        int grupa = 1;
+        String email = "a@b.com";
+        Student varga = new Student(id,nume,grupa,email);
+        service.addStudent(varga);
+        assert(service.findStudent("15").equals(varga));
+
+        Tema testTema = new Tema("100","ala bala portocala", 15,3);
+        try {
+            service.addTema(testTema);
+        }catch (ValidationException v){
+            assert(v.getMessage().equals("Deadlineul trebuie sa fie intre 1-14."));
+        }
+    }
+
+    @Test
+    public void testAddStudentAddAssignmentAndAddGradeException(){
+        String id = "15";
+        String nume = "varga";
+        int grupa = 1;
+        String email = "a@b.com";
+        Student varga = new Student(id,nume,grupa,email);
+        service.addStudent(varga);
+        assert(service.findStudent("15").equals(varga));
+
+        Tema testTema = new Tema("100","ala bala portocala", 13,3);
+        service.addTema(testTema);
+
+        Nota nota = new Nota("98","23","10",10.1, LocalDate.now());
+        
+        try {
+            service.addNota(nota,"iannis e un zeu");
+        }catch (ValidationException v){
+            assert(v.getMessage().equals("Valoarea notei nu este corecta!"));
+        }
     }
 
 
